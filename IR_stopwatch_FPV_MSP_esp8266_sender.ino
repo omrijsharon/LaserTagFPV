bad2aca1a98e1b4f1c454d310119a1a0183e1f1d
Omri J. Sharon <omri_s@yahoo.com>
2023-01-04 09:14:53 +0200

    Add files via upload

---
+++ IR_stopwatch_FPV_MSP_esp8266_sender.ino
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+
+const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+
+IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
+
+// Example of data captured by IRrecvDumpV2.ino
+uint16_t rawData[67] = {9000, 4500, 650, 550, 650, 1650, 600, 550, 650, 550,
+                        600, 1650, 650, 550, 600, 1650, 650, 1650, 650, 1650,
+                        600, 550, 650, 1650, 650, 1650, 650, 550, 600, 1650,
+                        650, 1650, 650, 550, 650, 550, 650, 1650, 650, 550,
+                        650, 550, 650, 550, 600, 550, 650, 550, 650, 550,
+                        650, 1650, 600, 550, 650, 1650, 650, 1650, 650, 1650,
+                        650, 1650, 650, 1650, 650, 1650, 600};
+
+void setup() {
+  pinMode(2, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+
+  irsend.begin();
+  digitalWrite(2, LOW);  // Turn the LED off by making the voltage HIGH
+}
+
+void loop() {
+  irsend.sendRaw(rawData, 67, 38);  // Send a raw data capture at 38kHz.
+  delay(10);
+}
